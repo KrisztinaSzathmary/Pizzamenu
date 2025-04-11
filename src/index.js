c8866ae579a8750a09c549f9400129696ef7f231
Krisztina Szathmary <krisztina.szathmary@codecentric.de>
2025-04-11 15:46:38 +0200

    Conditionaly set ccs class on sold out pizza

--- src/index.js
+++ src/index.js
@@ -76,11 +76,18 @@ function Menu() {
       <h2>Our menu</h2>
 
       {numPizzas > 0 ? (
-        <ul className="pizzas">
-          {pizzas.map((pizza) => (
-            <Pizza pizzaObject={pizza} key={pizza.name} />
-          ))}
-        </ul>
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delicious.
+          </p>
+
+          <ul className="pizzas">
+            {pizzas.map((pizza) => (
+              <Pizza pizzaObject={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
       ) : (
         <p>We're still working on our menu. Plesase come back later.</p>
       )}
@@ -91,15 +98,16 @@ function Menu() {
 function Pizza({ pizzaObject }) {
   console.log(pizzaObject);
 
-  if (pizzaObject.soldOut) return null;
+  //if (pizzaObject.soldOut) return null;
 
   return (
-    <li className="pizza">
+    <li className={`pizza ${pizzaObject.soldOut ? 'sold-out' : ''}`}>
       <img src={pizzaObject.photoName} alt={pizzaObject.name} />
       <div>
         <h3>{pizzaObject.name}</h3>
         <p>{pizzaObject.ingredients}</p>
-        <span>{pizzaObject.price}</span>
+
+        <span>{pizzaObject.soldOut ? 'SOLD OUT' : pizzaObject.price}</span>
       </div>
     </li>
   );
